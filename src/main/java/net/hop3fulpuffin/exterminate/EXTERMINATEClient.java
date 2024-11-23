2d79dff410fdf994c4f7c08dd3c6e3ec86761caf
hop3fulpuffin <blurryshurtugal@gmail.com>
2024-11-23 16:59:33 -0500

    yay

---
+++ src/main/java/net/hop3fulpuffin/exterminate/EXTERMINATEClient.java
@@ -0,0 +1,10 @@
+package net.hop3fulpuffin.exterminate;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EXTERMINATEClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
