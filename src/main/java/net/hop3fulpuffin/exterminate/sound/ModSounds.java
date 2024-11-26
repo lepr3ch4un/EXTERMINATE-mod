02bc5aa710b37987268236090271b057d0f481fb
hop3fulpuffin <blurryshurtugal@gmail.com>
2024-11-26 12:35:43 -0500

    some kind of sound bug

--- src/main/java/net/hop3fulpuffin/exterminate/sound/ModSounds.java
+++ src/main/java/net/hop3fulpuffin/exterminate/sound/ModSounds.java
@@ -16,6 +16,7 @@ public class ModSounds {
     public static final SoundEvent ENTITY_CYBERMAN_HURT = registerSoundEvent("entity.cyberman.hurt");
     public static final SoundEvent ENTITY_CYBERMAN_DEATH = registerSoundEvent("entity.cyberman.death");
 
+
     private static SoundEvent registerSoundEvent(String name) {
         Identifier id = new Identifier(EXTERMINATE.MOD_ID, name);
         return Registry.register(Registries.SOUND_EVENT, id, SoundEvent.of(id));
